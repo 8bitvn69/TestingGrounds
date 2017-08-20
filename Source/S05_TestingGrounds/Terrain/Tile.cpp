85e4f64fc5811e979ca8e9a33b8c0e8cc47e9b2f
Phùng Trọng Hiếu <8bitvn69@gmail.com>
2017-08-20 09:42:35 +0700

    Spawning Actors in the Tile

--- Source/S05_TestingGrounds/Terrain/Tile.cpp
+++ Source/S05_TestingGrounds/Terrain/Tile.cpp
@@ -12,14 +12,17 @@ ATile::ATile()
 
 }
 
-void ATile::PlaceActors() {
+void ATile::PlaceActors(TSubclassOf<AActor> ToSpawn, int32 MinSpawn, int32 MaxSpawn) {
 	FVector Min(0.f, -2000.f, 0.f);
 	FVector Max(4000.f, 2000.f, 0.f);
 	FBox Bounds(Min, Max);
-	
-	for (int i = 0; i < 20; i++) {
+	int32 NumberToSpawn = FMath::RandRange(MinSpawn, MaxSpawn);
+
+	for (int i = 0; i < NumberToSpawn; i++) {
 		FVector SpawnPoint = FMath::RandPointInBox(Bounds);
-		UE_LOG(LogTemp, Warning, TEXT("Spawn point: %s"), *SpawnPoint.ToCompactString())
+		AActor* Spawned = GetWorld()->SpawnActor<AActor>(ToSpawn);
+		Spawned->SetActorRelativeLocation(SpawnPoint);
+		Spawned->AttachToActor(this, FAttachmentTransformRules(EAttachmentRule::KeepRelative, false));
 	}
 }
 
--- Source/S05_TestingGrounds/Terrain/Tile.h
+++ Source/S05_TestingGrounds/Terrain/Tile.h
@@ -15,7 +15,7 @@ public:
 	ATile();
 
 	UFUNCTION(BlueprintCallable, Category = "Customs")
-	void PlaceActors();
+	void PlaceActors(TSubclassOf<AActor> ToSpawn, int32 MinSpawn, int32 MaxSpawn);
 
 protected:
 	// Called when the game starts or when spawned
